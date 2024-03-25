abf8bf2383460905852282320ffc7dab0dc658b4
TitusKI <kidumazie@gmail.com>
2024-03-25 11:38:08 +0200

    SignIn, SignUp setUserData on FireBase

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:user_repository/src/models/models.dart';
@@ -22,19 +24,39 @@ class FirebaseUserRepo implements UserRepository{
  
 
   @override
-  Future<void> signIn(String email, String password) {
-    throw UnimplementedError();
+  Future<void> signIn(String email, String password) async{
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
 
   @override
-  Future<MyUser> signUp(MyUser myUser, String password) {
-    throw UnimplementedError();
+  Future<MyUser> signUp(MyUser myUser, String password) async{
+      try {
+     UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(email: myUser.email, password: password);
+
+     myUser = myUser.copyWith(
+      userId: user.user!.uid
+     );
+     return myUser;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
  
   @override
-  Future<void> setUserData(MyUser myUser) {
+  Future<void> setUserData(MyUser myUser) async{
     
-    throw UnimplementedError();
+      try {
+      await userCollection.doc().set(myUser.toEntity().toDocument());
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
  
 
