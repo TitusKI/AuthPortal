196a999c2a5d1746329eb31109d8112979cb5517
TitusKI <kidumazie@gmail.com>
2024-03-25 11:38:59 +0200

    SignIn, SignUp setUserData on FireBase

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -7,26 +5,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:user_repository/src/models/models.dart';
 import 'package:user_repository/src/user_repository.dart';
 
-class FirebaseUserRepo implements UserRepository{
-
+class FirebaseUserRepo implements UserRepository {
   final userCollection = FirebaseFirestore.instance.collection('user');
   final FirebaseAuth _firebaseAuth;
-  FirebaseUserRepo({
-    FirebaseAuth? firebaseAuth
-  }):
-    _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
-   @override
-  Stream<User?> get user{
+  FirebaseUserRepo({FirebaseAuth? firebaseAuth})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  @override
+  Stream<User?> get user {
     return _firebaseAuth.authStateChanges().map((firebaseUser) {
       return firebaseUser;
     });
   }
- 
 
   @override
-  Future<void> signIn(String email, String password) async{
+  Future<void> signIn(String email, String password) async {
     try {
-      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
     } catch (e) {
       log(e.toString());
       rethrow;
@@ -34,30 +29,26 @@ class FirebaseUserRepo implements UserRepository{
   }
 
   @override
-  Future<MyUser> signUp(MyUser myUser, String password) async{
-      try {
-     UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(email: myUser.email, password: password);
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: myUser.email, password: password);
 
-     myUser = myUser.copyWith(
-      userId: user.user!.uid
-     );
-     return myUser;
+      myUser = myUser.copyWith(userId: user.user!.uid);
+      return myUser;
     } catch (e) {
       log(e.toString());
       rethrow;
     }
   }
- 
+
   @override
-  Future<void> setUserData(MyUser myUser) async{
-    
-      try {
+  Future<void> setUserData(MyUser myUser) async {
+    try {
       await userCollection.doc().set(myUser.toEntity().toDocument());
     } catch (e) {
       log(e.toString());
       rethrow;
     }
   }
- 
-
 }
