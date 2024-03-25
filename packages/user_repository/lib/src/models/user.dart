d71298f7cb175370c859eb9378fdc47d45842a3e
TitusKI <kidumazie@gmail.com>
2024-03-25 10:16:36 +0200

    ModelingUser

--- packages/user_repository/lib/src/models/user.dart
+++ packages/user_repository/lib/src/models/user.dart
@@ -13,23 +13,27 @@ class User extends Equatable {
   });
 
   static const empty = User(userId: '', email: '', name: '');
+  // copyWith methods for modifying the user variables
   User copyWith({String? userId, String? email, String? name}) {
     return User(
-        userId: userId ?? this.userId,
-        email: email ?? this.email,
-        name: name ?? this.name);
-  }
-  UserEntity toEntity(){
-    return UserEntity(
-       userId: userId,
-        email: email ,
-        name: name 
+      userId: userId ?? this.userId,
+      // this.userId is the one that is found in the User object
+      // that already have But userId is the one
+      // that is passed by the copyWith method
+      email: email ?? this.email,
+      name: name ?? this.name,
     );
   }
-static User fromEntity(UserEntity entity){
-  return User(userId: entity.userId, email: entity.email, name: entity.name);
-}
+// toEntity converts User Object to UserEntity Object
+  UserEntity toEntity() {
+    return UserEntity(userId: userId, email: email, name: name);
+  }
+// fromEntity converts UserEntity Object to User Object
+  static User fromEntity(UserEntity entity) {
+    return User(userId: entity.userId, email: entity.email, name: entity.name);
+  }
+
   @override
-  // TODO: implement props
+
   List<Object?> get props => [userId, email, name];
 }
