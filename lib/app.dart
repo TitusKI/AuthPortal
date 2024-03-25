70736510a6e14b5a3bdae431be736b32df16abca
TitusKI <kidumazie@gmail.com>
2024-03-25 12:25:29 +0200

    Relating Logic with Presentation

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
+
+
+class MyApp extends StatelessWidget {
+  final UserRepository userRepository;
+  const MyApp(this.userRepository, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
