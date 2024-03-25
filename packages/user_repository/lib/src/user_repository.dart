731fb8ab075cf93999e91746491b3338b2c1a85e
TitusKI <kidumazie@gmail.com>
2024-03-25 11:16:35 +0200

    AbstractClassToBeImplmentedByFirebaseRepository

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -1,9 +1,10 @@
-import 'package:MyUser_repository/src/models/models.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/models.dart';
 
 
 
-abstract class MyUserRepo{
+abstract class UserRepository{
 Stream<User?> get user;
 Future<void> signIn(String email, String password);
 Future<MyUser> signUp(MyUser myUser, String password);
