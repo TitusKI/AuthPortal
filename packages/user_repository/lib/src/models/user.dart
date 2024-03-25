9d85adb4b63a574e56a0114d2a10132c0b74c348
TitusKI <kidumazie@gmail.com>
2024-03-25 11:02:19 +0200

    ModelingUser

--- packages/user_repository/lib/src/models/user.dart
+++ packages/user_repository/lib/src/models/user.dart
@@ -1,39 +1,39 @@
 import 'package:equatable/equatable.dart';
-import 'package:user_repository/src/entities/user_entities.dart';
+import 'package:MyUser_repository/src/entities/user_entities.dart';
 
-class User extends Equatable {
-  final String userId;
+class MyUser extends Equatable {
+  final String MyUserId;
   final String email;
   final String name;
 
-  const User({
-    required this.userId,
+  const MyUser({
+    required this.MyUserId,
     required this.email,
     required this.name,
   });
 
-  static const empty = User(userId: '', email: '', name: '');
-  // copyWith methods for modifying the user variables
-  User copyWith({String? userId, String? email, String? name}) {
-    return User(
-      userId: userId ?? this.userId,
-      // this.userId is the one that is found in the User object
-      // that already have But userId is the one
+  static const empty = MyUser(MyUserId: '', email: '', name: '');
+  // copyWith methods for modifying the MyUser variables
+  MyUser copyWith({String? MyUserId, String? email, String? name}) {
+    return MyUser(
+      MyUserId: MyUserId ?? this.MyUserId,
+      // this.MyUserId is the one that is found in the MyUser object
+      // that already have But MyUserId is the one
       // that is passed by the copyWith method
       email: email ?? this.email,
       name: name ?? this.name,
     );
   }
-// toEntity converts User Object to UserEntity Object
-  UserEntity toEntity() {
-    return UserEntity(userId: userId, email: email, name: name);
+// toEntity converts MyUser Object to MyUserEntity Object
+  MyUserEntity toEntity() {
+    return MyUserEntity(MyUserId: MyUserId, email: email, name: name);
   }
-// fromEntity converts UserEntity Object to User Object
-  static User fromEntity(UserEntity entity) {
-    return User(userId: entity.userId, email: entity.email, name: entity.name);
+// fromEntity converts MyUserEntity Object to MyUser Object
+  static MyUser fromEntity(MyUserEntity entity) {
+    return MyUser(MyUserId: entity.MyUserId, email: entity.email, name: entity.name);
   }
 
   @override
 
-  List<Object?> get props => [userId, email, name];
+  List<Object?> get props => [MyUserId, email, name];
 }
