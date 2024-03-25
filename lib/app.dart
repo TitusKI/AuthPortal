39075314824c4514b5a7c6109ebe4bb6aeff2f2c
TitusKI <kidumazie@gmail.com>
2024-03-25 15:40:22 +0200

    Making BlocObseverclass

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
+import 'package:authenticateme/app_view.dart';
+import 'package:authenticateme/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
-
 
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
@@ -8,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider<AuthenticationBloc>(
+      create: (context) => AuthenticationBloc(userRepository: userRepository),
+      child: const MyAppView(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:authenticateme/app.dart';
+import 'package:authenticateme/simple_bloc_observer.dart';
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import "package:user_repository/user_repository.dart";
@@ -6,6 +8,7 @@ import "package:user_repository/user_repository.dart";
 void main() async {
   WidgetsFlutterBinding.ensureInitialized;
   await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObsever();
   runApp(MyApp(FirebaseUserRepo()));
 }
 
