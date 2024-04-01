75b50a2efaacc5f3656da52971d2fd9cfb532628
TitusKI <kidumazie@gmail.com>
2024-04-01 21:28:53 +0200

    Adding the ForgetPassword Method to the Abstract Class

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:user_repository/src/models/models.dart';
+import 'package:user_repository/user_repository.dart';
 
 abstract class UserRepository {
   Stream<User?> get user;
@@ -7,4 +8,5 @@ abstract class UserRepository {
   Future<MyUser> signUp(MyUser myUser, String password);
   Future<void> setUserData(MyUser myUser);
   Future<void> logOut();
+  Future<void> forgetPassword(String email);
 }
